0dc2107fc8dd2ff11f2ebd460daba1ffce36bb24
Sean Collins <sean.collins@outlook.com>
2023-06-19 16:52:45 -0700

    Fix syntax and spacing

--- cmd/root.go
+++ cmd/root.go
@@ -8,10 +8,10 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use: "jif",
+	Use:   "jif",
 	Short: "An opinionated CLI tool for interacting with the Jira API",
-	Long: "An opinionated CLI tool for interacting with the Jira API",
-	RunE: func(cmd &cobra.Command, args []string) error {
+	Long:  "An opinionated CLI tool for interacting with the Jira API",
+	RunE: func(cmd *cobra.Command, args []string) error {
 		return cmd.Help()
 	},
 }
