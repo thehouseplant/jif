913fb491eb9f1166f4be58df41cb7a956c3c273a
Sean Collins <sean.collins@outlook.com>
2023-06-19 18:05:05 -0700

    Mock writing a JSON file during init command

--- cmd/init.go
+++ cmd/init.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
 	"github.com/spf13/cobra"
 )
 
@@ -9,10 +13,24 @@ var initCmd = &cobra.Command{
 	Short: "Initialize a new Jif workspace",
 	Long:  `Initialize a new Jif workspace`,
 	Run: func(cmd *cobra.Command, args []string) {
-		//
+		writeConfiguration()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(initCmd)
+}
+
+type Config struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Team     string `json:"team"`
+	Sprint   string `json:"sprint"`
+}
+
+func writeConfiguration() {
+	fmt.Println("Writing configuration...")
+
+	file, _ := json.MarshalIndent("", "", " ")
+	_ = ioutil.WriteFile("config.json", file, 0644)
 }
